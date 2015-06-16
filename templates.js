2c97452b829387dd81170233479108979a4f84af
MeesterMeetch <mwhunt3@gmail.com>
2015-06-16 19:56:27 -0400

    _template for the results, which will be on the results page

---
+++ templates.js
@@ -0,0 +1,24 @@
+var templates = {};
+
+templates.placeMeResult = [
+"<div class='resultHeader col-md-12'>",
+"<%= name %>",
+"</div>",
+"<div class='resultsContent col-md-12'>",
+"<div id='resultsImage'class='col-md-7'>",
+"<img src=<%= image %> alt='' />",
+"</div>",
+"<div id='resultsText'class='col-md-5'>",
+"<ul>",
+"<li class='name'><%= name %></li>",
+"<li class='size'><%= size %></li>",
+"<li class='activities'><%= activities %></li>",
+"<li class='food'><%= food %></li>",
+"<li class='climate'><%= climate %></li>",
+"<li class='geography'><%= geography %></li>",
+"<li class='sports'><%= sports %></li>",
+"<li class='shopping'><%= shopping %></li>",
+"</ul>",
+"</div>",
+"</div>"
+].join("");
