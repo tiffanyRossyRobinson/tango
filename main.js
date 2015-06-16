21d7d731a38fd4864f87fe2dcf7bd730cc949cfe
tiffanyRossyRobinson <trossys@gmail.com>
2015-06-16 18:25:46 -0400

    this takes all the users input and turns it into an array

--- main.js
+++ main.js
@@ -5,6 +5,8 @@ $(document).ready(function(){
 
 var page ={
 
+  yourObject: {},
+
   init:function(arguments){
     page.initStyling();
     page.initEvents();
@@ -28,7 +30,38 @@ var page ={
 
   },
   captureData: function(){
-    console.log('you want to submit your data');
+    var name= $('.name').val();
+    var city= $('.city').val().toLowerCase();
+    var mysize= $('.mySize').val();
+    var climate= $('.myClimate').val();
+    var food= $('.myFood').val();
+    var shop = $('.myShop').val();
+    var sports= $('.mySports').val();
+    var geography= [];
+    var activities= [];
+    $('.findMatch input:checked').each(function() {
+      geography.push(this.value);
+    });
+
+
+    $('.myActivities input:checked').each(function() {
+        activities.push(this.value);
+    });
+
+    page.yourObject= {
+      name: name,
+      city: city,
+      size: mysize,
+      activities: activities,
+      food: food,
+      climate: climate,
+      geography: geography,
+      sports: sports,
+      shopping: shop
+    };
+
+    console.log(page.yourObject);
+
   }
 
 
