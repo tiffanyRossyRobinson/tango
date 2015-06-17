242d691d0f5c9ae0108461198d3c645016c76677
MeesterMeetch <mwhunt3@gmail.com>
2015-06-17 09:09:57 -0400

    stashing template

--- main.js
+++ main.js
@@ -5,6 +5,8 @@ $(document).ready(function(){
 
 var page ={
 
+  yourObject: {},
+
   init:function(arguments){
     page.initStyling();
     page.initEvents();
@@ -17,13 +19,50 @@ var page ={
 
   initEvents: function(arguments){
     $('#beginButton').on('click', page.openQuiz);
-
+  },
+  function(arguments){
+    $('.submit').on('click', page.captureData);
   },
 
   openQuiz: function(){
     $('.landingPage').removeClass('active');
     $('.landingPage').addClass('hide');
     $('.quizPage').addClass('active');
+
+  },
+  captureData: function(){
+    var name= $('.name').val();
+    var city= $('.city').val().toLowerCase();
+    var mysize= $('.mySize').val();
+    var climate= $('.myClimate').val();
+    var food= $('.myFood').val();
+    var shop = $('.myShop').val();
+    var sports= $('.mySports').val();
+    var geography= [];
+    var activities= [];
+    $('.findMatch input:checked').each(function() {
+      geography.push(this.value);
+    });
+
+
+    $('.myActivities input:checked').each(function() {
+        activities.push(this.value);
+    });
+
+    page.yourObject= {
+      name: name,
+      city: city,
+      size: mysize,
+      activities: activities,
+      food: food,
+      climate: climate,
+      geography: geography,
+      sports: sports,
+      shopping: shop
+    };
+
+    console.log(page.yourObject);
+
   }
 
 
